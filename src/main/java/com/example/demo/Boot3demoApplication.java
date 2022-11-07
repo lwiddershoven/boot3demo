a01c6613c16cedadba0fc69d1383ba26f191a5a7
lwiddershoven <lwiddershoven>
2022-11-07 16:49:42 +0100

    Added a "/" handler

--- src/main/java/com/example/demo/Boot3demoApplication.java
+++ src/main/java/com/example/demo/Boot3demoApplication.java
@@ -2,7 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
 @SpringBootApplication
 public class Boot3demoApplication {
 
@@ -10,4 +20,8 @@ public class Boot3demoApplication {
 		SpringApplication.run(Boot3demoApplication.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> home() {
+			return route(GET("/"), request -> ok().body(fromValue("Home")));
+	}
 }
--- src/test/java/com/example/demo/Boot3demoApplicationTests.java
+++ src/test/java/com/example/demo/Boot3demoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Boot3demoApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void loadIndex() {
+		var x = webTestClient.get().uri("/").exchange().expectBody(String.class).returnResult();
+		assertEquals("Home", x.getResponseBody());
 	}
 
 }
